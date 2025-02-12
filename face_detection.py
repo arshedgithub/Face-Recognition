48139a763d8d8ba6c47734a3feceb96e503a6c26
arshedgithub <arshedahmedmail@gmail.com>
2025-02-13 02:05:03 +0530

    collecting 30 photos

--- face_detection.py
+++ face_detection.py
@@ -3,22 +3,32 @@ import os
 
 haar_file = "haarcascade_frontalface_default.xml"
 datasets = 'datasets'
-sub_data = 'arshed'
+sub_data = 'elon'
 
 path = os.path.join(datasets, sub_data)
-print(path)
-
+if not os.path.isdir(path):
+    print(path)
+    os.mkdir(path)
+    
+(width, height) = (130, 100)
+    
 haar_cascade = cv2.CascadeClassifier(haar_file)
 camera = cv2.VideoCapture(0)
 
-while True:
+count = 1
+while count < 31:
+    print(count)
     _, frame = camera.read()
     
     grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    face = haar_cascade.detectMultiScale(grayImg, 1.3, 4) # get coordinates of face
+    faces = haar_cascade.detectMultiScale(grayImg, 1.3, 4) # get coordinates of face
     
-    for (x, y, w, h) in face:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = grayImg[y:y+h, x:x+w]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite('%s/%s-%s.png' % (path, sub_data, count), face_resize)
+        count += 1
         
     cv2.imshow("Face Detection", frame)
     key = cv2.waitKey(10) & 0xFF
